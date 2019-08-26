67506e5b0b80bc27daaf3fda6ac8597dbc31b704
Taylar Mouton <tmouton1@jh.edu>
2019-08-26 17:54:13 -0400

    Corrected Excerise 1

--- day1-homework/day1-exercise-1.py
+++ day1-homework/day1-exercise-1.py
@@ -5,6 +5,9 @@ import sys
 in_file = open(sys.argv[1])
 counter = 0
 for line in in_file: 
+    fields = line.split("\t")
+    if fields[2] == "*":
+        continue
     counter += 1
     #count = count + 1
 print(counter)
