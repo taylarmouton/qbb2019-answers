2ad870d898bafe2c733ad567191a546f5c1c0348
Taylar Mouton <tmouton1@jh.edu>
2019-08-29 22:41:06 -0400

    Final problems from D4 HW w/out cleaned up comments

---
+++ day4-homework/ma-plot.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+"""
+Usage: ./ma-plot.py <ctab1> <ctab2>
+./03-timecourse.py FBtr0331261 ~/qbb2019/data/samples.csv all.csv
+Create a time course of a given transcript for feamles and males
+
+../results/stringtie/SRR72...
+../results/stringtie SRR072916 SRR072915 SRR072914 
+"""
+#you just have to import two ctab files
+
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+
+name1 = sys.argv[1].split(os.sep)[-2]
+ctab1 = pd.read_csv(sys.argv[1], sep="\t", index_col="t_name")
+
+name2 = sys.argv[2].split(os.sep)[-2]
+ctab2 = pd.read_csv(sys.argv[2], sep="\t", index_col="t_name")
+
+#fpkm = {"sample1" : [1, 2, 3]
+#        "sample2" : [4, 5, 6]}
+
+fpkm = {name1 : ctab1.loc[:,"FPKM"],
+        name2 : ctab2.loc[:,"FPKM"]}
+
+df = pd.DataFrame(fpkm)
+df += 1
+
+#print(df)
+#print(type(df))
+
+r =df.loc[:,name2]
+g =df.loc[:,name1]
+m = np.log2(r/g)
+a = .5 * np.log2(r*g)
+
+fig, ax = plt.subplots()
+ax.scatter( x=a, y=m, s = 3, alpha = .3)
+ax.set_title("SRR072893 and SRR072894")
+ax.set_xlabel("m")
+ax.set_ylabel("a")
+fig.savefig("ma-plot.png")
+plt.close(fig)
+
---
+++ day4-homework/replicates.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+"""
+Usage: ./02-timecourse.py <t_name> <samples.csv> <FPKMs.>
+./03-timecourse.py FBtr0331261 ~/qbb2019/data/samples.csv all.csv
+Create a time course of a given transcript for feamles and males
+
+
+"""
+
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+#Specify transctipt of interest 
+t_name = sys.argv[1]
+
+#Load Metadata
+samples = pd.read_csv(sys.argv[2])
+ctab_dir = sys.argv[5]
+
+#Load Metadata
+def sex_sorter(sex):
+    soi = samples.loc[:,"sex"] == sex
+    stages = samples.loc[soi, "stage"]
+
+    #print(srr_ids)
+
+    #Load FPKMS
+    fpkms = pd.read_csv(sys.argv[3], index_col="t_name")
+
+    #Extract Data
+    my_data = []
+    for stage in stages:
+        #print(srr_id)
+        my_data.append(fpkms.loc[t_name,sex+ '_' +stage])
+    return my_data
+
+def sex_replicate(sex):
+    samples = pd.read_csv(sys.argv[4])
+    soi = samples.loc[:,"sex"] == sex
+    srr_ids = samples.loc[soi, "sample"] 
+    my_data = []     
+    for srr_id in srr_ids:
+        # my_data(my_data.loc[t_name, srr_id])
+        #print(srr_id)
+        #my_data.append(fpkms.loc[t_name,srr_id])
+
+        ctab_path = os.path.join(ctab_dir, srr_id,
+                                "t_data.ctab")
+        #print(ctab_path)
+        df = pd.read_csv(ctab_path, sep="\t", 
+                        index_col="t_name")
+        # my_data["gene_name"] = df.loc[:,"gene_name"]
+        my_data.append(df.loc[t_name, "FPKM"])
+    return my_data
+
+#Print my_data
+#print(my_data)
+
+male_data = sex_sorter("male")
+male_data_rep = sex_replicate("male")
+female_data = sex_sorter("female")
+female_data_rep = sex_replicate("female")
+labelz = ["male", "female"]
+labelz2 = ["10", "11", "12", "13", "14", "14A", "14B", "14C", "14D"]
+#Plot
+fig, ax = plt.subplots()
+plt.title("Male and Female Data for Sxl Transcripts")
+plt.xlabel("Embryonic Stage")
+plt.ylabel("mRNA Abundance")
+ax.plot(male_data, color="blue")
+ax.plot(female_data, color="red")
+ax.plot(range(4,8), male_data_rep, 'o', color="blue")
+ax.plot(range(4,8), female_data_rep, 'o', color="red")
+plt.legend(labels= labelz, loc= "center left", bbox_to_anchor=(1, 0.5))
+plt.tight_layout()
+ax.set_xticklabels(labelz2, rotation='vertical')
+ax.set_xticks(np.arange(len(labelz2)))
+fig.savefig("timecourse_male_female.png")
+plt.close(fig)
+
+
+#label = "mu=%f;sigma=%f;a=%f" %(mu,sigma,a)
