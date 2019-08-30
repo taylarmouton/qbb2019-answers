b286b03025d2d316203a63b29e6f5217479eb27b
Taylar Mouton <tmouton1@jh.edu>
2019-08-30 13:29:09 -0400

    Updated code for promoter start

--- day5-lunch/00-transcription-for-SRR.py
+++ day5-lunch/00-transcription-for-SRR.py
@@ -2,7 +2,10 @@
 
 """Usage: 
 
-././00-transcription-for-SRR.py <t_data.ctab> 
+./00-transcription-for-SRR.py <t_data.ctab> 
+transcrpt_SRR072893.bed
+
+/Users/cmdb/qbb2019-answers/day5-lunch $ bigWigAverageOverBed H3K4me1.bw transcrpt_SRR072893.bed 
 
 """
 import sys
@@ -17,16 +20,24 @@ all_start_end_values = ctab.loc[:,["chr", "t_name", "strand", "start", "end"]]
 #all_start_end_values["promoter start"] = ""
 
 #Labels
-print("Chr" + "\t" + "t_name" + "\t" + "promoter start" + "\t" + "promoter end")
+#print("Chr" + "\t" + "t_name" + "\t" + "promoter start" + "\t" + "promoter end")
 #Iterating throws in dataframe
 for index, row in all_start_end_values.iterrows():
    if row.loc["strand"] == "+":
        promoterStart = int(row.loc["start"])
        promoter_start_plus_500 = promoterStart + 500
        promoter_start_minus_500 = promoterStart - 500
-       print(str(row.loc["chr"]) + "\t" + str(row.loc["t_name"]) + "\t" + str(promoter_start_minus_500) + "\t" + str( promoter_start_plus_500))
+       if promoter_start_plus_500 <= 0:
+           promoter_start_plus_500 = 1
+       if promoter_start_minus_500 <= 0:
+           promoter_start_minus_500 = 1
+       print(str(row.loc["chr"]) + "\t" + str(promoter_start_minus_500) + "\t" + str( promoter_start_plus_500) + "\t" + str(row.loc["t_name"]))
    elif row.loc["strand"] == "-":
        promoterStart = int(row.loc["end"])
        promoter_start_plus_500 = promoterStart + 500
        promoter_start_minus_500 = promoterStart - 500
-       print(str(row.loc["chr"]) + "\t" + str(row.loc["t_name"]) + "\t" + str(promoter_start_minus_500) + "\t" + str( promoter_start_plus_500))
+       if promoter_start_plus_500 <= 0:
+           promoter_start_plus_500 = 1
+       if promoter_start_minus_500 <= 0:
+           promoter_start_minus_500 = 1
+       print(str(row.loc["chr"]) + "\t" + str(promoter_start_minus_500) + "\t" + str( promoter_start_plus_500) + "\t" + str(row.loc["t_name"]))
